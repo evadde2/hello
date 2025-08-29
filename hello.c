f1c693dde65c0dc045ad629f3f549c0ed688741c
jcolman936 <jcolman936@gmail.com>
2025-08-29 13:33:58 -0500

    Added my own name and major

--- hello.c
+++ hello.c
@@ -10,10 +10,14 @@ evadde2@huskers.unl.edu
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("Hello World, Esha Vadde!\n");
     printf("My major is Computer Science!\n");
     printf("I like taking walks\n");
+    printf("I am Jaedyn Colman\n");
+    printf("I'm majoring in Computer Science!\n");
+
+
 
     return 0;
 }
